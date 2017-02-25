57323a64d687156e7476dbd23201e7ebd3c90b06
Joe Haines <hello@joehaines.co.uk>
2017-02-25 18:44:54 +0000

    Add hover style

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -6,9 +6,14 @@ export default styled.a`
   color: ${subtle};
   text-decoration: none;
   border-bottom: 1px solid ${subtle};
+  transition: border-bottom-color 0.25s ease-in-out;
+
+  &:hover {
+    border-bottom-color: ${primary};
+  }
 
   &:active {
     color: ${primary};
-    border-color: ${primary};
+    border-bottom-color: ${primary};
   }
 `
